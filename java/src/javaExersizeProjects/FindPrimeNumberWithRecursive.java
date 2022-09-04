c2f7279569a12f5d8aee06fc06938c6e65690ed8
ISA-KAYABASI <isakayabasi30@gmail.com>
2022-09-04 10:21:17 +0300

    recursive prime number

--- java/src/javaExersizeProjects/FindPrimeNumberWithRecursive.java
+++ java/src/javaExersizeProjects/FindPrimeNumberWithRecursive.java
@@ -1,4 +1,28 @@
 package javaExersizeProjects;
 
+import java.util.Scanner;
+
 public class FindPrimeNumberWithRecursive {
+
+        static void primeNumber(int number, int x){
+
+            if (x == number){
+                System.out.println(number + " sayisi asal sayidir.");
+                return;
+            }else if (number % x == 0){
+                System.out.println(number + "  sayisi asal sayi degildir.");
+                return ;
+            }
+            primeNumber(number,x + 1);
+
+        }
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Please enter a number : ");
+            int number=scanner.nextInt();
+            primeNumber(number,2);
+        }
 }
+
