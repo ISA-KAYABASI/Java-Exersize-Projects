90cd77efac96773578b793882bb25cf08dcde528
ISA-KAYABASI <isakayabasi30@gmail.com>
2022-09-04 13:00:44 +0300

    Student app

---
+++ java/src/javaExersizeProjects/StudentManagmentSystem/Teacher.java
@@ -0,0 +1,14 @@
+package javaExersizeProjects.StudentManagmentSystem;
+
+public class Teacher {
+
+    String name;
+    String mpno;
+    String branch;
+
+    public Teacher(String name, String mpno, String branch) {
+        this.name = name;
+        this.mpno = mpno;
+        this.branch = branch;
+    }
+}
