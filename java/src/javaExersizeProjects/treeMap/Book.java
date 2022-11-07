a4f35f52cf1cc87d46f5d05f289ef0833aa8e18c
ISA-KAYABASI <isakayabasi30@gmail.com>
2022-11-07 16:51:16 +0300

    TreeSet usage with example

---
+++ java/src/javaExersizeProjects/treeMap/Book.java
@@ -0,0 +1,53 @@
+package javaExersizeProjects.treeMap;
+
+public class Book implements Comparable<Book> {
+
+    public String name;
+    public int pages;
+    public String author;
+    public int publicationDate;
+
+    public Book(String name, int pages, String author, int publicationDate) {
+        this.name = name;
+        this.pages = pages;
+        this.author = author;
+        this.publicationDate = publicationDate;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+        return getName().compareTo(o.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(int publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+}
---
+++ java/src/javaExersizeProjects/treeMap/Main.java
@@ -0,0 +1,57 @@
+package javaExersizeProjects.treeMap;
+
+import java.util.Comparator;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<Book> book = new TreeSet<>();
+
+        Book book1 = new Book("Kasik Yurek ",220,"Ahmet Yilmaz ",1995);
+        Book book2 = new Book("Cesur Yurek ",230,"Kelly Tutson ",1945);
+        Book book3 = new Book("Aman Tanrim ",240,"Jessi Barton ",1955);
+        Book book4 = new Book("Gezgin      ",250,"Halit Sacip  ",1895);
+        Book book5 = new Book("Seker Kazani",260,"Jonny Larmoda",1935);
+
+        book.add(book1);
+        book.add(book2);
+        book.add(book3);
+        book.add(book4);
+        book.add(book5);
+
+        System.out.println("Sort By Name : ");
+        for (Book bookName:book){
+            System.out.println(" Book Name : "+bookName.getName()
+            +" Book Page : "+bookName.getPages()
+            +" Book Author : "+bookName.getAuthor()
+            +" Book Publish Date : "+bookName.getPublicationDate());
+        }
+
+        TreeSet<Book> books=new TreeSet<>(new Comparator<Book>() {
+            @Override
+            public int compare(Book o1, Book o2) {
+                return o1.getPages()-o2.getPages();
+            }
+        });
+
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+
+        System.out.println("**************");
+        System.out.println("Books sorted by number of pages");
+        for (Book bookPage:books) {
+            System.out.println("Book Name : " + bookPage.getName()+
+                    "Book Page : " + bookPage.getPages()+
+                    "Book Author : " + bookPage.getAuthor()+
+                    "Book Publish Date : " + bookPage.getPublicationDate());
+        }
+
+
+
+
+
+
+    }
+}
